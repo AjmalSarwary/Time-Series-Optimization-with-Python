221e1cba050e0c6debb0505ebcfe29176d46b22d
ajmalsarwary <sarwary.ey@outlook.com>
2024-01-16 15:36:06 +0100

    extended by 3 additional functions

--- code/invest_risk_kit.py
+++ code/invest_risk_kit.py
@@ -111,4 +111,28 @@ def cvar_historic(r, level=5):
     else:
         raise TypeError("Expected r to be Series or DataFrame")
 
+def annualize_rets(r, periods_per_year):
+    """
+        Annualizes set of returns
+    """
+    compounded_growth = (1+r).prod()
+    n_periods = r.shape[0]
+    return compounded_growth**(periods_per_year/n_periods)-1
 
+def annualize_vol(r, periods_per_year):
+    """
+    Annualizes the volatility of a set of returns 
+    """
+    # scale the std by the number of the sqrt of the periods
+    return r.std()*(periods_per_year**0.5)
+
+def sharpe_ratio(r, riskfree_rate, periods_per_year):
+    """
+    Computes the annualized sharpe ratio of a set of returns
+    """
+    #convert the annual riskfree rate to per period
+    rf_per_period = (1+riskfree_rate)**(1/periods_per_year)-1
+    excess_ret = r - rf_per_period
+    ann_ex_ret = annualize_rets(excess_ret, periods_per_year)
+    ann_vol = annualize_vol(r, periods_per_year)
+    return ann_ex_ret/ann_vol
