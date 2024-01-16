40761841c2a4123e0923cca435329934e72fce6b
ajmalsarwary <sarwary.ey@outlook.com>
2024-01-16 18:28:13 +0100

    added plot_ef2 function

--- code/invest_risk_kit.py
+++ code/invest_risk_kit.py
@@ -150,4 +150,17 @@ def portfolio_vol(weights, covmat):
     Weights -> Volatility
     """
     return (weights.T  @ covmat @ weights)**0.5
+
+
+def plot_ef2(n_points, er, cov, style):
+    """
+    Plots the 2-asset efficient frontier
+    """
+    if er.shape[0] !=2:
+        raise ValueError("plot_ef2 can only plot 2-asset frontiers")
+    weights = [np.array([w,1-w]) for w in np.linspace(0,1,n_points)]
+    rets = [portfolio_return(w, er) for w in weights]
+    vols = [portfolio_vol(w, cov) for w in weights]
+    ef = pd.DataFrame({"Returns": rets, "Vols": vols})
+    return ef.plot.line(x="Vols", y="Returns", style=style)
     
